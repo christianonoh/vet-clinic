c0d7d3346a46e0a9b7721423f2fea935312c5aad
Christian Onoh <chibyk5000@gmail.com>
2023-06-08 19:39:23 +0100

    Revert "Update table"

--- schema.sql
+++ schema.sql
@@ -4,7 +4,6 @@ CREATE DATABASE vet_clinic;
 
 USE vet_clinic;
 
-DROP TABLE IF EXISTS animals;
 CREATE TABLE animals (
   id INT  GENERATED ALWAYS AS IDENTITY,
   name VARCHAR(25) NOT NULL,
@@ -14,27 +13,3 @@ CREATE TABLE animals (
   weight_kg DECIMAL,
   PRIMARY KEY (id)
 );
-
-ALTER TABLE animals ADD species VARCHAR(40);
-
-DROP TABLE IF EXISTS owners;
-CREATE TABLE owners (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  full_name VARCHAR(50) NOT NULL,
-  age INT,
-  PRIMARY KEY(id)
-);
-
-DROP TABLE IF EXISTS species;
-CREATE TABLE species (
-  id INT GENERATED ALWAYS AS IDENTITY,
-  name VARCHAR(40) NOT NULL,
-  PRIMARY KEY(id)
-);
-
-ALTER TABLE animals DROP COLUMN species;
-ALTER TABLE animals ADD species_id INT;
-ALTER TABLE animals ADD CONSTRAINT FK_AnimalSpecies FOREIGN KEY (species_id) REFERENCES species(id);
-
-ALTER TABLE animals ADD owner_id INT;
-ALTER TABLE animals ADD CONSTRAINT FK_AnimalOwner FOREIGN KEY (owner_id) REFERENCES owners(id);
